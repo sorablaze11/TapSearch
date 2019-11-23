08dfb12f89e47e47595096aec6e67f3122763561
Sukh Raj Limbu <limburaj65@gmail.com>
2019-11-23 09:38:52 +0530

    Updates

---
+++ precache-manifest.a2ff1ea85d400dd2f7500a0f82d10dca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6795658d7e8c142a00cdc6c85edd9b6",
+    "url": "/sorablaze11/TapSearch/index.html"
+  },
+  {
+    "revision": "c51a6dad31c4f95833cd",
+    "url": "/sorablaze11/TapSearch/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "69e735253b70c81e009b",
+    "url": "/sorablaze11/TapSearch/static/js/2.8560bfd1.chunk.js"
+  },
+  {
+    "revision": "c51a6dad31c4f95833cd",
+    "url": "/sorablaze11/TapSearch/static/js/main.94802eea.chunk.js"
+  },
+  {
+    "revision": "bb7aac3877f8a23d356d",
+    "url": "/sorablaze11/TapSearch/static/js/runtime-main.9f9ae81a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sorablaze11/TapSearch/precache-manifest.a2ff1ea85d400dd2f7500a0f82d10dca.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sorablaze11/TapSearch/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
