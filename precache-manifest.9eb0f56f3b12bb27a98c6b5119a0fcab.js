cde842811f85acaff8d56bb6445acdee91699ce3
Sukh Raj Limbu <limburaj65@gmail.com>
2019-11-23 09:46:43 +0530

    Updates

---
+++ precache-manifest.9eb0f56f3b12bb27a98c6b5119a0fcab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f04e05214904ed673a89a6129819bba",
+    "url": "/sorablaze11/TapSearch/index.html"
+  },
+  {
+    "revision": "7eabc25cb08e7ed310cb",
+    "url": "/sorablaze11/TapSearch/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "69e735253b70c81e009b",
+    "url": "/sorablaze11/TapSearch/static/js/2.8560bfd1.chunk.js"
+  },
+  {
+    "revision": "7eabc25cb08e7ed310cb",
+    "url": "/sorablaze11/TapSearch/static/js/main.95327d33.chunk.js"
+  },
+  {
+    "revision": "bb7aac3877f8a23d356d",
+    "url": "/sorablaze11/TapSearch/static/js/runtime-main.9f9ae81a.js"
+  }
+]);
